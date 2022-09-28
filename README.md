e68dd2133d9ecd06c3e069037d7fce950a6dd109
yogeshnivi <109645156+yogeshnivi@users.noreply.github.com>
2022-09-28 16:02:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yoginivi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tjwsma)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1></h1>`;
+function login() {
+  alert('login successfully');
+}
